2e6ad6852768cd3311cd224c6c01e92c191cec66
LaTaerra <LaTaerra@mail.ru>
2016-05-21 11:10:58 +0800

    New Function

--- prog.c
+++ prog.c
@@ -11,3 +11,21 @@ int main()
 	float a  = 0;
 	float b  = 0;
 	float c  = 0;
+	printf( " a*x^2 + b*x + c = 0\n " );
+
+	printf( " input a =\n " );
+	scanf( "%f", &a );
+
+	printf( " input b =\n " );
+	scanf( "%f", &b );
+
+	printf( " input c =\n " );
+	scanf( "%f", &c );
+
+	printf( "(%.2f)*x^2 + (%.2f)*x + (%.2f) = 0 \n", a, b, c );
+
+	function(a, b, c);
+	
+	return 0;
+
+}
